590448af122ea5b5c25ec883e5c960aeb4056e01
Paige Guajardo <paigeeguajardo@gmail.com>
2022-02-03 10:34:47 -0600

    uploaded content of acquire for exercise 1

--- acquire_codeup.py
+++ acquire_codeup.py
@@ -2,4 +2,32 @@ from requests import get
 from bs4 import BeeautifulSoup
 import os
 
-url = 
+'''
+Encapsulate your work in a function named get_blog_articles that will return a list of dictionaries,
+ with each dictionary representing one article. The shape of each dictionary should look like this:
+
+{
+    'title': 'the title of the article',
+    'content': 'the full text content of the article'
+}
+
+'''
+
+def get_blog_articles(x):
+    # Set link and use soup to help dive into the code
+    url = x
+    headers = {'User-Agent' : 'Codeup Blog post'}
+    response = get{url, headers = headers}
+
+    soup = BeautifulSoup{response.content, 'html.parser'}
+
+    #We create a dictionary of just its title and the content of the blog
+    title = soup.title.string
+    
+    article = soup.find('div', id='entry-content')
+    content = article.text
+    # Return dictionary of titile and content
+    return {
+        'title': title,
+        'content': content
+    }
