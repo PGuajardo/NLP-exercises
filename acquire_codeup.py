6f4b96e5c8caaac6dc73e53537b080a4afc64a00
Paige Guajardo <paigeeguajardo@gmail.com>
2022-02-03 17:06:58 -0600

    Updated prep for cleaning/stemming/lemming, Updated data_prep_nlp for cleaning/stemming/lemming process on both news articles and codeupblog posts

--- acquire_codeup.py
+++ acquire_codeup.py
@@ -1,5 +1,7 @@
-from requests import get
+import requests
+from time import strftime
 from bs4 import BeautifulSoup
+import pandas as pd
 import os
 
 '''
@@ -13,21 +15,73 @@ Encapsulate your work in a function named get_blog_articles that will return a l
 
 '''
 
-def get_blog_articles(x):
+def get_front_page_articles():
     # Set link and use soup to help dive into the code
-    url = x
-    headers = {'User-Agent' : 'Codeup Blog post'}
-    response = get(url, headers = headers)
+    headers = {"User-Agent" : "Codeup Blogs"}
+    response = requests.get("https://codeup.com/blog/", headers = headers)
 
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.text)
 
-    #We create a dictionary of just its title and the content of the blog
-    title = soup.title.string
-    
-    article = soup.find('div', id='entry-content')
-    content = article.text
-    # Return dictionary of titile and content
+    links = [link.attrs["href"] for link in soup.select(".more-link")]
+
+    return links
+
+
+def parse_blogs(url):
+    #Given a blog url, extract info and return as a dictonary
+    response = requests.get(url, headers = {"User-Agent": "Codeup Blog"})
+    soup = BeautifulSoup(response.text)
     return {
-        'title': title,
-        'content': content
+        "title": soup.select_one(".entry-title").text,
+        "published": soup.select_one(".published").text,
+        "content": soup.select_one(".entry-content").text.strip()
     }
+
+def get_blog_articles():
+    # Return a datafram where each row is a blog post from codeup
+    links = get_front_page_articles()
+    df = pd.DataFrame([parse_blogs(link) for link in links])
+
+    return df
+
+
+# This is for inshorts news articles!
+#--------------------------------------------------
+
+def parse_news_card(card):
+    # returns infor abouts a specific news card
+    card_title = card.select_one('.news-card-title')
+
+    output = {}
+    output['title'] = card.find('span', itemprop = 'headline').text
+    output['author'] = card.find('span', class_ = 'author').text
+    output['content'] = card.find('div', itemprop = 'articleBody').text
+    output['date'] = card.find('span', clas = 'date').text
+    return output
+
+
+def parse_inshort_page(url):
+    # given a url, returna  dataframe
+
+    category = url.split('/')[-1]
+    response = requests.get(url, headers = {'user-agent' : 'codeup inshorts'})
+    soup = BeautifulSoup(response.text)
+    cards = soup.select('.news-card')
+    df = pd.DataFrame([parse_news_card(card) for card in cards])
+    df['category'] = category
+    return df
+
+def get_inshorts_articles():
+    # return dataframe of the news articles in inshorts for business/ sports/ technology/ and entertainment sections
+    url = 'https://inshorts.com/en/read/'
+    categories = ['business', 'sports', 'technology', 'etertainment']
+    df = pd.DataFrame()
+    for cat in categories:
+        df = pd.concat([df, pd.DataFrame(parse_inshort_page(url + cat))])
+    
+    df = df.reset_index(drop = True)
+    return df
+
+def save_as_json():
+    today = strftime('%Y-%m-%d')
+    return get_inshorts_articles().to_json(f'inshorts-{today}.json')
---
+++ prepare_codeup.py
@@ -0,0 +1,68 @@
+import unicodedata
+import re
+import json
+
+import nltk
+from nltk.tokenize.toktok import ToktokTokenizer
+from nltk.corpus import stopwords
+
+import pandas as pd
+import acquire_codeup
+
+
+#acquire_codeup.get_blog
+
+
+def basic_clean(x):
+    #lowercase
+    x = x.lower()
+   
+    #normalize unicode characters
+    #normalize removes any inconsistencies in unicode character
+    # .encode converts the resulting stirng to ascii character set, we ingore any errors in the conversions (we drop any non ascii characters)
+    # .decode turn resulting bytes into string
+    x = unicodedata.normalize('NFKD', x)\
+        .encode('ascii', 'ignore')\
+        .decode('utf-8', 'ignore')
+
+    #replace anyting that is not a letter, number, whitespace or a single quote
+    x = re.sub(r"[^a-z0-9'\s]", '', x)
+    return x
+
+
+def tokenize(x):
+    tokenizer = nltk.tokenize.ToktokTokenizer()
+
+    return print(tokenizer.tokenize(x , return_str = True))
+
+
+
+def stem(x):
+    ps = nltk.porter.PorterStemmer()
+
+    stems = [ps.stem(word) for word in x.split()]
+    aritcle_stemmed = ' '.join(stems)
+    return aritcle_stemmed
+
+
+
+def lemmatize(x):
+    wnl = nltk.stem.WordNetLemmatizer()
+    lemmas = [wnl.lemmatize(word) for word in x.split()]
+
+    article_lemmatized = ' '.join(lemmas)
+    return article_lemmatized
+
+
+def remove_stopwords(extra_words, exclude_words, x):
+    # setting the list to english
+    stopword_list = stopwords.words('english')
+
+    stopword_list = [stopword_list.append(i) for i in extra_words]
+    stopword_list = [stopword_list.remove(i) for i in exclude_words]
+
+    words = x.split()
+    filtered_words = [w for w in words if w not in stopword_list]
+
+    article_without_stopwords = ' '.join(filtered_words)
+    return article_without_stopwords
